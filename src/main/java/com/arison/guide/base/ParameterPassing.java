096704f6666cbfad7cc1a63cbaca4a035dec3a82
Arison <784602719@qq.com>
2019-10-23 16:43:00 +0800

    java基础知识系统学习笔记-提交10-23

--- src/main/java/com/arison/guide/base/ParameterPassing.java
+++ src/main/java/com/arison/guide/base/ParameterPassing.java
@@ -1,14 +1,54 @@
 package com.arison.guide.base;
 
 
+import com.arison.guide.model.Point;
 import com.arison.guide.model.Student;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 //值传递和引用传递
 public class ParameterPassing {
 
     public static void main(String[] args) {
-        testQuoteTypeAgain();
+        run01();
     }
+
+    /**
+     *
+     */
+    public static void run01(){
+        int a=1;
+        String b="hello";
+        Point p=new Point(a,2);
+        Collection c=new ArrayList();
+        c.add(p);
+        test01(a,b,p,c);
+
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+        System.out.println("p:"+p.toString());
+        System.out.println("c:"+c.toString());
+    }
+
+    public static void test01(int a, String b, Point p, Collection c){
+        a=a+1;
+        b=b+"world";
+        p.setX(3);
+
+        p=new Point(5,6);
+        p.setX(7);
+
+        c.clear();;
+        c.add(p);
+
+        c=new ArrayList();
+        c.add(new Point(9,0));
+
+        p.setX(a);
+    }
+
 
     /**
      * 基本数据类型-值传递
